44c74f660c36dde811eaf1f6fd28cb1765301774
juanma <juanma@gmail.com>
2025-04-21 18:25:54 +0200

    Ejercicio 1 de js hecho

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const valor1 = prompt("Dame el primer valor");
+const valor2 = prompt("Dame el segundo valor");
+
+if(valor1 > valor2){
+	console.log(`Valor 1 es mayor, y es el ${valor1}`);
+}else{
+	console.log(`Valor 2 es mayor, y es el ${valor2}`);	
+}
